47f63e65dd0ebea3618d7e77d7487462bab58a26
amogha_k <111457107+Amogha-k@users.noreply.github.com>
2024-08-10 15:40:03 +0530

    Delete server/server directory

--- server/server/src/main/java/com/nitte_job/server/constants/APIConstants.java
+++
@@ -1,8 +0,0 @@
-package com.nitte_job.server.constants;
-
-public class APIConstants {
-
-    public static final String GET_ALL_POSTS = "/posts";
-
-    public static final String SAVE_POST = "/post";
-}
--- server/server/src/main/java/com/nitte_job/server/dao/PostDao.java
+++
@@ -1,7 +0,0 @@
-package com.nitte_job.server.dao;
-
-import com.nitte_job.server.model.PostModel;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface PostDao extends MongoRepository<PostModel, String> {
-}
